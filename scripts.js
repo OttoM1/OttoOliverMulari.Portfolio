8e25ab5d2ed12078c728fc4e230d74c6d19e6258
OttoM1 <omulari24@student.oulu.fi>
2025-01-21 03:14:58 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
+/*
+function toggleSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.classList.remove('visible'); // Remove the visible class from all sections
+        section.classList.remove('fade-in-up'); // Remove fade-up animation class
+    });
+
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.classList.add('visible'); // Add the visible class to the selected section
+        setTimeout(() => {
+            section.classList.add('fade-in-up'); // Add fade-up animation after making it visible
+        }, 10);  // Slight delay to ensure the element is shown before animation starts
+    }
+}
+
+// Show the "About Me" section by default
+document.addEventListener('DOMContentLoaded', () => {
+    toggleSection('about-me');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent default anchor click behavior
+        const targetId = this.getAttribute('href').substring(1);
+        toggleSection(targetId);
+    });
+});
+*/
 function toggleSection(sectionId) {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
