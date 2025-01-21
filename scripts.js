8ef6480c4c786a423892403aac9caa7a3a516771
OttoM1 <omulari24@student.oulu.fi>
2025-01-21 03:09:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 function toggleSection(sectionId) {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
-        section.classList.remove('fade-in-up'); // Remove fade-up animation class from all sections
+        section.classList.remove('visible'); // Remove the visible class from all sections
+        section.classList.remove('fade-in-up'); // Remove fade-up animation class
     });
 
     const section = document.getElementById(sectionId);
     if (section) {
-        section.classList.add('visible'); // Make the section visible
+        section.classList.add('visible'); // Add the visible class to the selected section
         setTimeout(() => {
             section.classList.add('fade-in-up'); // Add fade-up animation after making it visible
         }, 10);  // Slight delay to ensure the element is shown before animation starts
